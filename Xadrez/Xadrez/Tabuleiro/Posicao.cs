019f9f87309acd8da167c4e173865f2363d26b6e
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-17 14:25:36 -0300

    Criacao de Nova Classe Posicao

---
+++ Xadrez/Xadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.Tabuleiro;
+
+class Posicao
+{
+    public int Linha { get; set; }
+    public int Coluna { get; set; }
+}
