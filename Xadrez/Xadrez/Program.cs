6167d4562192c372118f9614df3a2496e72d80ee
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-18 16:17:27 -0300

    CriandoPosicoes

--- Xadrez/Xadrez/Program.cs
+++ Xadrez/Xadrez/Program.cs
@@ -7,13 +7,9 @@ class program
 {
     static void Main(string[] args)
     {
-        Tabuleiro tab = new Tabuleiro(8, 8);
-
-        tab.InserirPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-        tab.InserirPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-        tab.InserirPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-        Tela.ImprimirTabuleiro(tab);
+        PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+        Console.WriteLine(pos);
+        Console.WriteLine(pos.toPosicao()); 
     }
 }
 
--- Xadrez/Xadrez/Tabuleiro/Tabuleiro.cs
+++ Xadrez/Xadrez/Tabuleiro/Tabuleiro.cs
@@ -22,10 +22,39 @@ class Tabuleiro
     {
         return pecas[linha, coluna];
     }
+    public Peca peca(Posicao pos)
+    {
+        return pecas[pos.linha, pos.coluna];
+    }
+    public bool ExistePeca(Posicao pos)
+    {
+        ValidarPosicao(pos);
+        return peca(pos) != null;
+    }
+
     public void InserirPeca(Peca p, Posicao pos)
     {
+        if (ExistePeca(pos)) 
+        {
+            throw new TabuleiroException("Já existe peça nesta posição");
+        }
         pecas[pos.linha, pos.coluna] = p;
         p.posicao = pos;
+    }
+    public bool Posicaovalida(Posicao pos) 
+    { 
+        if(pos.linha < 0 || pos.linha >= linhas || pos.coluna <0 || pos.coluna >= colunas) 
+        {
+            return false;
+        }
+        return true;    
+    }
+    public void ValidarPosicao(Posicao pos) 
+    {
+        if(!Posicaovalida(pos))
+        {
+            throw new TabuleiroException("Posição Invalida!");
+        }
     }
 
 }
