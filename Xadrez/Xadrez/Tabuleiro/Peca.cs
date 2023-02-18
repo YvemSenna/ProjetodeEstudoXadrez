1a42c408c61763a8f5932d5ebfacfeabbc734031
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-17 22:55:52 -0300

    ImplementacaoTabuleiro

---
+++ Xadrez/Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.Tabuleiro;
+
+class Peca
+{
+    public Posicao posicao { get; set; }
+    public Cor cor { get; protected set; }
+    public int QteMovimentos { get; protected set; }
+    public Tabuleiro tab { get; protected set; }
+
+    public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+    {
+        this.posicao = posicao;
+        this.cor = cor;
+        this.tab = tab;
+        this.QteMovimentos= 0;
+    }
+}
